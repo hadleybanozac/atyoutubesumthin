735ca120e4374db74098a466b035ccc2a1382752
Rachael Thiel <rachaelthiel@Rachaels-MacBook-Pro.local>
2016-12-14 01:17:10 -0600

    more css, player controls, cool stuff

---
+++ controls.js
@@ -0,0 +1,30 @@
+function skip1(){
+	var skip1 = document.getElementById('skip1')
+    skip1.onclick = function(){
+      player.seekTo(0);
+    }
+}
+
+function skip2(){
+	player.seekTo(30)
+}
+
+function skip3(){
+	player.seekTo(60)
+}
+
+function skip4(){
+	player.seekTo(90)
+}
+
+function skip5(){
+	player.seekTo(120)
+}
+
+function skip6(){
+	player.seekTo(150)
+}
+
+
+
+
